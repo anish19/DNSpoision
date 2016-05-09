b297d2c76bd060a1cdc7457f1909eed9d5ba675c
anish19 <hoopanish@gmail.com>
2016-05-09 00:39:10 -0400

    almost done

--- dnsdetect.py
+++ dnsdetect.py
@@ -1,135 +1,65 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 from scapy.all import *
-import sys
+import netifaces
 import getopt
 import socket
-from collections import deque
-from time import ctime
 import datetime
 
+changeToIp = netifaces.ifaddresses('eth0')[2][0]['addr']
+interface = ""
+filename = ""
+BPFfilter = ""
+recordTable = {}
+fflag = 0
 
-def main():
+def arp_monitor_callback(pkt):
+	ip = pkt['IP']
+        udp = pkt['UDP']
+	dns = pkt['DNS']
+	responseList = []
+	for i in range(dns.ancount):
+		dnsrr = dns.an[i]
+		responseList.append(dnsrr.rdata)
+	if dns.id in recordTable:
+		for ipans in responseList:
+			if ipans not in recordTable[dns.id]:
+				print datetime.datetime.now(), "\t", "DNS poisioning attempt"
+				print "TXID ", hex(dns.id), " Request ", dnsrr.rrname
+				print "Answer1 ", recordTable[dns.id]
+				print "Answer2 ", dnsrr.rdata
+				print '\n'
+				return
+	else:
+		recordTable[dns.id] = responseList
+	return
+	
+interfaces = netifaces.interfaces()
+interface = interfaces[1]
+opts, remainder = getopt.getopt(sys.argv[1:], "i:r:")
 
-    interfaceFlag = 0
-    fileFlag = 0
-    expressionFlag = 0
-    #interface = "ens33" #default interface
-    try:
-        opts, expressionList = getopt.getopt(sys.argv[1:], "i:r:")
-    except getopt.GetoptError as err:
-        print str(err)
-        print "Usage: sudo python dnsdetect.py [-i interface] [-r tracefile] expression"
-        sys.exit(2)
+for opt, arg in opts:
+	if opt in ('-i'):
+		interface = arg
+	elif opt in ('-r'):
+		filename = arg
 
-    for option, value in opts:
-        if option == "-i":
-            interfaceFlag = 1
-            interface = value
-        if option == "-r":
-            fileFlag = 1
-            fileName = value
+BPFfilter = " ".join(str(x) for x in remainder)
 
-    if (fileFlag and interfaceFlag):
-        print "Error! -i option cannot be used with -r option!"
-        sys.exit(2)
-    if (fileFlag and expressionFlag):
-        print "Error! Expression cannot be used with -r option!"
-        sys.exit(2)
+print "interface is ", interface
 
-    if expressionList != []:
-        expressionFlag = 1
+if len(filename) > 0:
+	fflag = 1
+	print "filename is ", filename
 
-    if fileFlag:
-        #get the packets from the pcap file
-        try:
-            pktList = rdpcap(fileName)
-        except Exception as err:
-            print str(err)
-            sys.exit(2)
+if len(BPFfilter) > 0: 
+	print "BPFfilter is ", BPFfilter
+	if "src port 53" in BPFfilter:
+		print "Cannot enter src port 53 as a BPF filter"
+		print "Please try again"
+		exit()
+if fflag is 0:
+	sniff(iface=interface,prn=arp_monitor_callback, filter="src port 53", store=0)
+else:
+	sniff(offline=filename,prn=arp_monitor_callback, filter="src port 53", store=0)
+	
 
-        #print len(pktList)
-        masterDNSpktList = []
-        for pkt in pktList:
-            DNSpktList = []
-            rdataList = []
-            if (pkt.haslayer(DNSRR)) and not(pkt.haslayer(ICMP)):
-                DNSpktList.append(format(pkt.getlayer(DNS).id, '#04x'))
-                DNSpktList.append(pkt.getlayer(DNS).qd.qname.rstrip("."))
-                DNSframe = pkt['DNS']
-                for i in xrange(DNSframe.ancount):
-                    if DNSframe.an[i].type == 1 or DNSframe.an[i].type == 28:
-                        rdataList.append(DNSframe.an[i].rdata)
-                DNSpktList.append(rdataList)
-                masterDNSpktList.append(DNSpktList)
-
-        #The master list is ready. Find the attack
-        #print masterDNSpktList
-        for i in range(0, len(masterDNSpktList)-1):
-            for j in range(i+1, len(masterDNSpktList)):
-                if (masterDNSpktList[i][0] == masterDNSpktList[j][0]) and \
-                        (masterDNSpktList[i][2] != masterDNSpktList[j][2]):
-                    print
-                    print
-                    print "!!!!! ALERT !!!!!"
-                    print ctime(), now.isoformat(), "DNS poisoning attempt!"
-                    print "TXID:", masterDNSpktList[i][0], "Request:", masterDNSpktList[i][1]
-                    print "Answer1:", masterDNSpktList[i][2]
-                    print "Answer2:", masterDNSpktList[j][2]
-
-    else: #Detect spoof packets by live capture
-
-        #set the bpf filter
-        expression = ''
-        for exp in expressionList:
-            expression = expression + " " + exp
-        expression = expression.strip(" ")
-        if (expressionFlag):
-            bpfFilter = "src port 53 and " + "(" + expression + ")"
-        else:
-            bpfFilter = "src port 53"
-
-        print "Detecting DNS poisoning..."
-        masterDNSpktList = deque([])
-        while (1):
-            try:
-                if interfaceFlag:
-                    pktSniffed = sniff(iface = interface, filter = bpfFilter, count = 10)
-                else:
-                    pktSniffed = sniff(filter = bpfFilter, count = 10)
-            except Exception as err:
-                print str(err)
-                sys.exit(2)
-            for pkt in pktSniffed:
-                DNSpktList = []
-                rdataList = []
-                if (pkt.haslayer(DNSRR)) and not(pkt.haslayer(ICMP)):
-                    DNSpktList.append(format(pkt.getlayer(DNS).id, '#04x'))
-                    DNSpktList.append(pkt.getlayer(DNS).qd.qname.rstrip("."))
-                    DNSframe = pkt['DNS']
-                    for i in xrange(DNSframe.ancount):
-                        if DNSframe.an[i].type == 1 or DNSframe.an[i].type == 28:
-                            rdataList.append(DNSframe.an[i].rdata)
-                    DNSpktList.append(rdataList)
-                    poisonFlag = 0
-                    #Check if packet is a duplicate packet
-                    if (len(masterDNSpktList) > 0):
-                        for DNSpkt in masterDNSpktList:
-                            if (DNSpktList[0] == DNSpkt[0]) and (DNSpktList[2] != DNSpkt[2]):
-                                print
-                                print
-                                print "!!!!! ALERT !!!!!"
-                                print "DNS poisoning attempt!"
-                                print "TXID:", DNSpktList[0], "Request:", DNSpktList[1]
-                                print "Answer1:", DNSpkt[2]
-                                print "Answer2:", DNSpktList[2]
-                                poisonFlag = 1
-                                break
-                        if (poisonFlag == 0):
-                            if (len(masterDNSpktList) == 50):
-                                masterDNSpktList.popleft()
-                            masterDNSpktList.append(DNSpktList)
-                    else:
-                        masterDNSpktList.append(DNSpktList)
-
-if __name__ == "__main__":
-    main()
